fa67657be2ae265ac46ee3ffe6b805067317a8de
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-02-26 17:39:25 -0300

    (feat) Se crea chat desde cliente

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>Websocket - Client</h1>
     <span id="server-status">offline</span>
+    <ul id="clients-ul"></ul>
+    <form id="message-form">
+      <input placeholder="message" id="message-input" />
+    </form>
   </div>
 `;
 
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -8,16 +8,41 @@ export const connectToServer = () => {
   addListeners(socket);
 };
 
-
 const addListeners = (socket: Socket) => {
-  const serverStatusLabel = document.querySelector('#server-status');
+  const serverStatusLabel = document.querySelector("#server-status");
+  const clientsUl = document.querySelector("#clients-ul");
 
-  socket.on('connect', () => {
-    serverStatusLabel!.innerHTML = 'connected';
+  const messageForm = document.querySelector<HTMLFormElement>("#message-form");
+  const messageInput =
+    document.querySelector<HTMLInputElement>("#message-input");
+
+  socket.on("connect", () => {
+    serverStatusLabel!.innerHTML = "connected";
   });
 
-  socket.on('disconnect', () => {
-    serverStatusLabel!.innerHTML = 'disconnected';
+  socket.on("disconnect", () => {
+    serverStatusLabel!.innerHTML = "disconnected";
   });
 
-}
+  socket.on("clients-updated", (clients: string[]) => {
+    let clientsHtml = "";
+    clients.forEach((clientId) => {
+      clientsHtml += `
+        <li>${clientId}</li>
+      `;
+    });
+    clientsUl!.innerHTML = clientsHtml;
+  });
+
+  messageForm!.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (messageInput!.value.trim().length <= 0) return;
+
+    socket.emit("message-from-client", {
+      id: "yo",
+      message: messageInput!.value,
+    });
+
+    messageInput!.value = '';
+  });
+};
