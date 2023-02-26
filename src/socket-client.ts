4a6c361c7973e65656e50d79673e13d358ec39f3
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-02-26 16:49:46 -0300

    (feat) Se cambia estado de conexion y desconexion app

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,11 +1,23 @@
-import { Manager } from "socket.io-client";
+import { Manager, Socket } from "socket.io-client";
 
 export const connectToServer = () => {
-
   const manager = new Manager("http://localhost:3000/socket.io/socket.io.js");
 
-  const socket = manager.socket('/'); // namespace a conectar, esta en el socket del server
+  const socket = manager.socket("/"); // namespace a conectar, esta en el socket del server
 
-  console.log({ socket });
-
+  addListeners(socket);
 };
+
+
+const addListeners = (socket: Socket) => {
+  const serverStatusLabel = document.querySelector('#server-status');
+
+  socket.on('connect', () => {
+    serverStatusLabel!.innerHTML = 'connected';
+  });
+
+  socket.on('disconnect', () => {
+    serverStatusLabel!.innerHTML = 'disconnected';
+  });
+
+}
